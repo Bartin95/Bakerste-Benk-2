81f28eb9e457cfd9c071a727fadacf1f083604ed
Glenn Haldorsen <glenn.haldorsen@gmail.com>
2018-09-19 20:55:55 +0200

    Stuff..

--- User-Story-3/src/java/classes/Tools.java
+++ User-Story-3/src/java/classes/Tools.java
@@ -6,7 +6,7 @@
 package classes;
 
 import java.io.PrintWriter;
-import java.sql.*; 
+import java.sql.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -17,61 +17,55 @@ import javax.sql.DataSource;
  * @author glenn
  */
 public class Tools {
-    Connection conn; 
+
+    Connection conn;
     Statement stmt;
-    
-     public void printUsers(PrintWriter out)
-    { 
-         String strSelect = "select * from users";
 
+    public void printUsers(PrintWriter out) {
+        String strSelect = "select * from users";
 
-         
-         System.out.println();
-         out.println();
-         
-         try {
-             
-                ResultSet rset = stmt.executeQuery(strSelect);
- 
-                // Step 4: Process the ResultSet by scrolling the cursor forward via next().
-                //  For each row, retrieve the contents of the cells with getXxx(columnName).
-                out.println("Students in class:" +"<br>");
-                int rowCount = 0;
-                while(rset.next()) {   // Move the cursor to the next row, return false if no more row
-                    String fname = rset.getString("fname");
-                    String lname = rset.getString("lname");
-                    String  cus_email  = rset.getString("cus_email");
-                    out.println(rowCount +": " + fname + ", " + lname + ", " + cus_email +"<br>");
-                    ++rowCount;
-                 }  // end while
-                 out.println("Number of students: " + rowCount);
-         } // end catch     
-         catch (SQLException ex) {
-                out.println("Ikke hentet fra DB " +ex);
-         }
-   }
-    
-    
-   // @Resource DataSource LocalhostDS;
+        System.out.println();
+        out.println();
+
+        try {
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+            //  For each row, retrieve the contents of the cells with getXxx(columnName).
+            out.println("Students in class:" + "<br>");
+            int rowCount = 0;
+            while (rset.next()) {   // Move the cursor to the next row, return false if no more row
+                String fname = rset.getString("fname");
+                String lname = rset.getString("lname");
+                String cus_email = rset.getString("cus_email");
+                out.println(rowCount + ": " + fname + ", " + lname + ", " + cus_email + "<br>");
+                ++rowCount;
+            }  // end while
+            out.println("Number of students: " + rowCount);
+        } // end catch     
+        catch (SQLException ex) {
+            out.println("Not from DB " + ex);
+        }
+    }
+
+    // @Resource DataSource LocalhostDS;
     public void loggInn2(PrintWriter out) {
         try {
-         // Step 1: Allocate a database 'Connection' object
-         Context cont = new InitialContext();
-         DataSource ds = (DataSource)cont.lookup("java:comp/env/jdbc/localhostDS");  
-         //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
-         conn =  ds.getConnection();
- 
-         // Step 2: Allocate a 'Statement' object in the Connection
-         stmt = conn.createStatement();
-        }
-        catch (SQLException ex ) {
-            out.println("Not connected to database " +ex);
-        }
-        catch (NamingException nex) {
+            // Step 1: Allocate a database 'Connection' object
+            Context cont = new InitialContext();
+            DataSource ds = (DataSource) cont.lookup("java:comp/env/jdbc/localhostDS");
+            //DataSource ds = (DataSource)cont.lookup("jdbc/LocalhostDS");
+            conn = ds.getConnection();
+
+            // Step 2: Allocate a 'Statement' object in the Connection
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            out.println("Not connected to database " + ex);
+        } catch (NamingException nex) {
             out.println("Not correct naming" + nex);
         }
-            
-            
+
     }
-    
+
 }
